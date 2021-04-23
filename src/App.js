ac70253d332a5066325df4a81c50a504d942b346
mohit58 <mohit.kumar@pantelwar.com>
2021-04-23 07:00:07 +0530

    firebase server & context api's added

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllMeetups from "./pages/AllMeetups";
+import Favorites from "./pages/Favorites";
+import NewMeetup from "./pages/NewMeetup";
+
+function App() {
+  return (
+    <Layout>
+      <Switch>
+        <Route exact path='/'><AllMeetups /></Route>
+        <Route path='/new-meetup'><NewMeetup /></Route>
+        <Route path='/favorites'><Favorites /></Route>
+      </Switch>
+    </Layout>
+  );
+}
+
+export default App;
